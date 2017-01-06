80e187ed2084798b76c4a846bccc16f33bc7a839
Alexander Armstrong <s154302@student.dtu.dk>
2017-01-06 06:49:24 +0100

    Init Day

--- src/classes/Day.java
+++ src/classes/Day.java
@@ -1,5 +1,36 @@
 package classes;
 
+import org.cmg.resp.behaviour.Agent;
+import org.cmg.resp.comp.Node;
+import org.cmg.resp.knowledge.ts.TupleSpace;
+
 public class Day {
 
+	public Day() {
+		
+		Node daySpace = new Node("day", new TupleSpace());
+		Agent dayAgent = new DayAgent("day");
+		
+		daySpace.addAgent(dayAgent);
+	}
+	
+	public static class DayAgent extends Agent{
+
+		public DayAgent(String name) {
+			super(name);
+			
+		}
+
+		@Override
+		protected void doRun() throws Exception {
+			
+			try{
+				
+			} catch(Exception e) {
+				
+			}
+			
+		}
+		
+	}
 }
