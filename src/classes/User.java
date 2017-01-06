d4b0716f5cd85ea33c047100ee443137e3a1457c
Emilie Dahl <s153762@student.dtu.dk>
2017-01-06 14:45:59 +0100

    Added features in UserClass: sign up to a day, sign up af chef, add day

--- src/classes/User.java
+++ src/classes/User.java
@@ -17,7 +17,7 @@ public class User {
 	protected static String userName;
 	protected static Node userSpace;
 	protected String kitchenName;
-	protected UserAgent userAgent;
+	//protected UserAgent userAgent;
 
 	/*
 	 * public User(String name){ //TODO - Lav user uden navn }
@@ -28,39 +28,62 @@ public class User {
 		this.userName = userName;
 		userSpace = new Node("UserSpace" + userName, new TupleSpace());
 		userSpace.addPort(Server.vp);
-		userAgent = new UserAgent(userName);
-		userSpace.addAgent(userAgent);
+		//userAgent = new UserAgent(userName);
+		//userSpace.addAgent(userAgent);
 		userSpace.start();
 	}
-
+	
+	// Adding new day to the Dinnerclub calender
+	public void addDay(int day, int mounth, int year){
+		AddDayAgent addDay = new AddDayAgent(day, mounth, year);
+		userSpace.addAgent(addDay);	
+	}
+	
+	// Assigning this user to the given day
+	public void signToDay(int day, int mounth, int year, int amount){
+		SignToDayAgent signToDay = new SignToDayAgent(day, mounth, year, amount);
+		userSpace.addAgent(signToDay);
+	}
+	
+	// Assigning this user as chef to a given day
+	public void signAsChef(int day, int mounth, int year, String menu){
+		SignAsChefAgent signAsChef = new SignAsChefAgent(day, mounth, year, menu);
+		userSpace.addAgent(signAsChef);
+	}
+	
+/*
 	public static class UserAgent extends Agent {
 
 		protected static PointToPoint p;
-
+		
 		public UserAgent(String UserName) {
 			super(UserName);
-
 		}
-
+		
 		@Override
 		protected void doRun() {
-
 		}
 	}
-	
+*/	
 	public static class AddDayAgent extends Agent {
 
 		protected static PointToPoint p;
 		protected int day,mounth,year;
 
 		public AddDayAgent(int day, int mounth, int year) {
-			super(""+day+mounth+year);
+			super("NewDay"+day+mounth+year);
 			p = new PointToPoint("Server Monitor", Server.vp.getAddress());
 			this.day = day;
 			this.mounth = mounth;
 			this.year = year;
-			
-
+			// necessary?
+			/*
+			try {
+				exec(this);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			*/
 		}
 
 		@Override
@@ -72,19 +95,89 @@ public class User {
 			try {
 				put(t,p);
 				get(template,p);
+			} catch (InterruptedException | IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static class SignToDayAgent extends Agent {
+
+		protected static PointToPoint p;
+		protected int day,mounth,year, amount;
+
+		public SignToDayAgent(int day, int mounth, int year, int amount) {
+			super("SignDay"+day+mounth+year);
+			p = new PointToPoint("Server Monitor", Server.vp.getAddress());
+			this.day = day;
+			this.mounth = mounth;
+			this.year = year;
+			this.amount = amount;
+			// necessary?
+			/*
+			try {
+				exec(this);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			*/
+		}
+
+		@Override
+		protected void doRun() {
+			Tuple newDay = new Tuple("New Day", day, mounth, year, amount, User.userName);
+			Tuple t = new Tuple("Server", newDay);
+			Template template = new Template(new ActualTemplateField(User.userName), new FormalTemplateField(Tuple.class));
+			
+			try {
+				put(t,p);
+				get(template,p);
 				
 			} catch (InterruptedException | IOException e) {
 				e.printStackTrace();
 			}
-			
-
 		}
 	}
 	
-	public void AddDay(int day, int mounth, int year){
-		AddDayAgent addDay = new AddDayAgent(day, mounth, year);
-		userSpace.addAgent(addDay);
+	public static class SignAsChefAgent extends Agent {
+
+		protected static PointToPoint p;
+		protected int day,mounth,year;
+		protected String menu;
+
+		public SignAsChefAgent(int day, int mounth, int year, String menu) {
+			super(menu+day+mounth+year);
+			p = new PointToPoint("Server Monitor", Server.vp.getAddress());
+			this.day = day;
+			this.mounth = mounth;
+			this.year = year;
+			this.menu = menu;
+			// necessary?
+			/*
+			try {
+				exec(this);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			*/
+		}
+
+		@Override
+		protected void doRun() {
+			Tuple chef = new Tuple("Chef", day, mounth, year, menu, User.userName);
+			Tuple t = new Tuple("Server", chef);
+			Template template = new Template(new ActualTemplateField(User.userName), new FormalTemplateField(Tuple.class));
+			
+			try {
+				put(t,p);
+				get(template,p);
+				
+			} catch (InterruptedException | IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
+
 
 }
