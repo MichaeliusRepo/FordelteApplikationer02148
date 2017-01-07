ead51eb3b4d8e6a746b682389cb76588464b9b89
MichaeliusDenModige <s143049@student.dtu.dk>
2017-01-07 20:46:56 +0100

    HORY SHITE IT WORKS \O/ VICTORY /applause

--- src/classes/Server.java
+++ src/classes/Server.java
@@ -16,7 +16,14 @@ import org.cmg.resp.topology.PointToPoint;
 import org.cmg.resp.topology.Self;
 import org.cmg.resp.topology.VirtualPort;
 
-@SuppressWarnings("unused") // insert that programmer joke here
+@SuppressWarnings("unused")
+// A guy is standing on the corner of the street smoking one cigarette after
+// another. A lady walking by notices him and says "Hey, don't you know that
+// those things can kill you? I mean, didn't you see the giant warning on the
+// box?!" "That's OK" says the guy, puffing casually, "I'm a computer
+// programmer" "So? What's that got to do with anything?" "We don't care about
+// warnings. We only care about errors."
+
 public class Server {
 
 	public static VirtualPort vp = new VirtualPort(1337); // 1337 h4x0r
@@ -27,12 +34,23 @@ public class Server {
 
 	private void initialize() {
 		Node server = new Node("Server", new TupleSpace());
+		server.addPort(vp);
 		Agent monitor = new Monitor("Server Monitor");
 		server.addAgent(monitor);
 		server.start();
+
+		// MOCK CODE
+		String kitchenName = "Den Store Bagedyst";
+		User NortiousMaximus = new User("Nortious Maximus", kitchenName);
+		Kitchen kitchen = new Kitchen(kitchenName);
+		NortiousMaximus.addDay(7, 1, 2017);
+
 	}
 
 	public static class Monitor extends Agent {
+
+		Tuple t;
+		Template what = new Template(new FormalTemplateField(String.class), new FormalTemplateField(Tuple.class));
 
 		public Monitor(String who) {
 			super(who);
@@ -40,37 +58,55 @@ public class Server {
 
 		@Override
 		protected void doRun() {
-			Tuple t;
-			Template what = new Template(new FormalTemplateField(String.class), new FormalTemplateField(Tuple.class));
-			try {
-				// t = query(what, Self.SELF);
-				// System.out.println(" " + name + " saw " +
-				// t.getElementAt(String.class, 1));
 
-				// This is an example of how to use getAll methods from jRESP
-				// LinkedList<Tuple> list = new LinkedList<Tuple>();
-				
-				// use this to copy all tuples
-				// list = queryAll(what);
-				
-				// use this to obtain all tuples
-				// list = getAll(what);
+			while (true) {
 
-				t = get(what, Self.SELF);
+				try {
+					// t = query(what, Self.SELF);
+					// System.out.println(" " + name + " saw " +
+					// t.getElementAt(String.class, 1));
 
-				switch (t.getElementAt(String.class, 0)) {
-				case "AddDay":
-					System.out.println("Check info from kitchen and day");
-					System.out.println("Send confirmation to the user.");
+					// This is an example of how to use getAll methods from
+					// jRESP
+					// LinkedList<Tuple> list = new LinkedList<Tuple>();
 
-				case "CreateKitchen":
-					System.out.println("Create Agent that will create a valid kitchen");
-					System.out.println("Send confirmation to the user. Command was handled successfully.");
+					// use this to copy all tuples
+					// list = queryAll(what);
+
+					// use this to obtain all tuples
+					// list = getAll(what);
+
+					t = get(what, Self.SELF);
+
+					switch (t.getElementAt(String.class, 0)) {
+					case "addDay":
+
+						Tuple tupleData = t.getElementAt(Tuple.class, 1);
+						System.out.println("Server Monitor saw date " + tupleData.getElementAt(Integer.class, 1) + "/"
+								+ tupleData.getElementAt(Integer.class, 2) + "/"
+								+ tupleData.getElementAt(Integer.class, 3));
+						
+						System.out.println("Pretend that I validated adding this date.");
+						System.out.println("Pretend that I added a day from serverside!");
+						
+						String userSpaceName = tupleData.getElementAt(String.class, 0);
+						PointToPoint p = new PointToPoint(userSpaceName, vp.getAddress());
+						put(new Tuple(userSpaceName,tupleData),p);
+						System.out.println("Server sent feedback.");
+
+						break;
+
+					case "CreateKitchen":
+						System.out.println("Create Agent that will create a valid kitchen");
+						System.out.println("Send confirmation to the user. Command was handled successfully.");
+						break;
+					}
+
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
-
-			} catch (Exception e) {
-				e.printStackTrace();
 			}
+
 		}
 	}
 
@@ -93,34 +129,44 @@ public class Server {
 
 }
 
-
 /*
  * 1. Uno!
+ * 
  * Create User
+ * 
  * Create Kitchen (creates budget automatically)
- * Create Day
- * Assign user to day (sign up to eating at a day)
+ * 
+ * Create Day Assign user to day (sign up to eating at a day)
  * 
  * 2. Deux
+ * 
  * Set cook on day (private)
+ * 
  * Get cook on day
  * 
  * 3. Trois
+ * 
  * Calculate who's cookin' (automatically distributes staffing)
  * 
  * 4. Quatre
+ * 
  * Set price on a day's cooking (in Day.class)
+ * 
  * Get price from day's cooking (send to user)
  * 
  * 5. Quins
+ * 
  * Get WHO and HOW MANY are signed up to a day.
  * 
  * 6. Six
+ * 
  * Make budget.
  * 
  * 7. Seben
+ * 
  * Close signups.
  * 
  * 8. Acht
+ * 
  * Handling of guests...?
  */
