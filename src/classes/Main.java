c4585ceec604725e1e0e9e6cfd05f89f6557c85f
Emilie Dahl <s153762@student.dtu.dk>
2017-01-09 15:39:50 +0100

    can run more commands from the console

--- src/classes/Main.java
+++ src/classes/Main.java
@@ -27,31 +27,45 @@ public class Main {
 			user = dinnerClub.getUser(userName);
 		}
 
-		for (int i = 0; i < 5; i++)
+		for (int i = 0; i < 10; i++)
 		{
-			System.out.println("Please enter your command: ");
+			System.out.println("Please enter 'day' if you want to add/edit a day: ");
 			String cmd = in.nextLine();
 
 			switch (cmd) {
 
-			case "Add Day":
+			case "day":
 				System.out.print("Enter day: ");
-				day = in.nextInt();
+				day = Integer.parseInt(in.nextLine());
 				System.out.print("Enter month: ");
-				month = in.nextInt();
+				month = Integer.parseInt(in.nextLine());
 				System.out.print("Enter year: ");
-				year = in.nextInt();
-				user.addDay(day, month, year);
-				break;
-				
-			case "Add Chef":
-				System.out.print("Enter day: ");
-				day = in.nextInt();
-				System.out.print("Enter month: ");
-				month = in.nextInt();
-				System.out.print("Enter year: ");
-				year = in.nextInt();
-				user.addChef(day, month, year);
+				year = Integer.parseInt(in.nextLine());
+				System.out.println("Please enter 'Add Day', 'Add Chef', 'Attend Day', 'Unattend Day' or 'Lock Day': ");
+				String dayCmd = in.nextLine();
+				System.out.println("you entered: "+ dayCmd);
+				switch(dayCmd){
+				case "Add Day":
+					System.out.println("adding day");
+					user.addDay(day, month, year);
+					break;
+					
+				case "Add Chef":
+					user.addChef(day, month, year);
+					break;
+
+				case "Attend Day":
+					user.attendDay(day, month, year);
+					break;
+					
+				case "Unattend Day":
+					user.unattendDay(day, month, year);
+					break;
+					
+				case "Lock Day":
+					user.lockDay(day, month, year);
+					break;	
+				}
 				break;
 
 			case "Get my budget":
