dd87df831b324557e04dbebfe35cea94c5a303c5
Emilie Dahl <s153762@student.dtu.dk>
2017-01-09 12:14:53 +0100

    Main: more than one command, and need correct username.

--- src/classes/Main.java
+++ src/classes/Main.java
@@ -5,27 +5,34 @@ import java.util.Scanner;
 public class Main {
 	
 	public static void main(String[] args){
-Server dinnerClub = new Server();
 		
+		Server dinnerClub = new Server();
+		User user = null;
 		Scanner in = new Scanner(System.in);
+		
+		while(user==null){
 	    System.out.print("Please enter user name: (write 'new user' to create new user) ");
-	    String userName = in.nextLine();      
+	    String userName = in.nextLine();  
+	    
 	    System.out.println("You entered: " + userName);
-	    if(userName.equals("new user")){
-	    	System.out.print("Please enter a new user name: ");
-		    userName = in.nextLine();      
-		    System.out.println("You entered: " + userName);
-		    System.out.print("Please enter a dinner club's name: ");
-		    String kitchenName = in.nextLine();
-	    	dinnerClub.newUser(userName,kitchenName);
-	    }
-	    User user = dinnerClub.getUser(userName);
+	    	if(userName.equals("new user")){
+	    		System.out.print("Please enter a new user name: ");
+	    		userName = in.nextLine();      
+	    		System.out.println("You entered: " + userName);
+	    		System.out.print("Please enter a dinner club's name: ");
+		    	String kitchenName = in.nextLine();
+		    	dinnerClub.newUser(userName,kitchenName);
+	    	}
+	    	user = dinnerClub.getUser(userName);
+		}
 	    
-	    if(user != null){
-	    System.out.print("Please enter your command: ");
-	    String cmd = in.nextLine();
+	  
+	    for(int i = 0; i<5; i++){
+		    System.out.print("Please enter your command: ");
+		    String cmd = in.nextLine();
 	    
-	    switch (cmd) {
+		    switch (cmd) {
+	   
 	    	case "Add Day":
 	    		System.out.print("Enter day: ");
 	    		int day = in.nextInt();
@@ -42,9 +49,10 @@ Server dinnerClub = new Server();
 	    	case "something":
 		    	break;
 	    }
-	    } else {
-	    	System.out.println("Bye");
-	    }
+	    	 
+	}
+	System.out.println("Buy");
+	    
 	}
 
 }
