48f6d051aba91f8068eca9fd42de7c49343b2f3f
Emilie Dahl <s153762@student.dtu.dk>
2017-01-11 12:07:54 +0100

    solved error in login laylot

--- src/application/Start.java
+++ src/application/Start.java
@@ -4,22 +4,25 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
 public class Start extends Application {
 	@Override
 	public void start(Stage primaryStage) {
+		
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Login.fxml"));
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Login.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Dinner Club");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+		
 	}
 	
 	public static void main(String[] args) {
