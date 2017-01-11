173d4a2d195a7933a0623f9bf0cbee7ad75b7419
JonRavnMacbook <JonRavnMacbook@jons-air.eduroam.wireless.dtu.dk>
2017-01-11 17:33:28 +0100

    Layout fixes

--- src/application/LoginController.java
+++ src/application/LoginController.java
@@ -2,10 +2,15 @@ package application;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+
+import java.io.IOException;
 
 import classes.Server;
 
@@ -32,12 +37,14 @@ public class LoginController {
     }
 
     @FXML
-    void login(ActionEvent event) {
+    void login(ActionEvent event) throws IOException {
     	String user = username.getText();
     	
     	if(DinnerClub.getUser(user) != null){
     		wrongUsername.setText("");
     		System.out.println("Welcome "+user);
+    //		Parent create_club_page = FXMLLoader.load(getClass().getResource("CreateDinnerClub.fxml"));
+    	//	Scene home_page_scene = new Scene(create_club_page);
     		// new window
     	} else {
     		wrongUsername.setText("  Unknown username");
