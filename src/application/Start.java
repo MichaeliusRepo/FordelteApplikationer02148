e3b582eca491af3d424caaed37cd8f673abbc1c4
Emilie Dahl <s153762@student.dtu.dk>
2017-01-18 14:14:25 +0100

    dialog box server ip

--- src/application/Start.java
+++ src/application/Start.java
@@ -1,5 +1,7 @@
 package application;
 	
+import java.util.Optional;
+
 import classes.Server;
 import classes.User;
 import javafx.application.Application;
@@ -8,6 +10,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextInputDialog;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
@@ -17,11 +20,22 @@ public class Start extends Application {
 	public void start(Stage primaryStage) {
 		
 		try {
+			User user = new User();
+			TextInputDialog dialog = new TextInputDialog("Server IP");
+			dialog.setTitle("Find Server");
+			dialog.setHeaderText("Where is the server?");
+			String contentText = "Write the server IP";
+			dialog.setContentText(contentText);
+			Optional<String> result = dialog.showAndWait();
+			if(result.isPresent()){
+				user.setServerIP(result.get());
+			}
+			
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/Login.fxml"));
 			Parent root = loader.load();
 			
 			LoginController controller = loader.getController();
-			controller.setUser(new User());
+			controller.setUser(user);
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setOnCloseRequest(new EventHandler <WindowEvent>() {
