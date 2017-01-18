f279b56815195073e4be2988f82cdc64cc8b7312
tk7000 <s154219@student.dtu.dk>
2017-01-18 15:07:38 +0100

    ijeioewjfi

---
+++ src/classes/ServerMain.java
@@ -0,0 +1,11 @@
+package classes;
+
+public class ServerMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Server server = new Server();
+		System.out.println("Server kører");
+	}
+
+}
